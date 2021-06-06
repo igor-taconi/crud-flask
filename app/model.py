e9671d60eb5cf665481465e90bb70673bad373ac
taconi <igortaconi1@gmail.com>
2021-06-06 08:57:44 -0300

    change sqlalchemy to peewee

--- app/model.py
+++ app/model.py
@@ -1,19 +1,16 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy.ext.declarative import declarative_base
+import peewee as pw
 
-engine = create_engine('sqlite:///users.db')
-session = scoped_session(sessionmaker(bind=engine, autoflush=False))
-Base = declarative_base()
+db = pw.SqliteDatabase('users.db')
+
+class BaseModel(pw.Model):
+    class Meta:
+        database = db
 
 
-class User(Base):
-    __tablename__ = 'User'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String(100), unique=True, nullable=False)
-    email = Column(String(100), unique=True, nullable=False)
-    password = Column(String(100), nullable=False)
+class User(BaseModel):
+    email = pw.CharField(unique=True)
+    password = pw.CharField()
+    username = pw.CharField()
 
     def __repr__(self):
         return f"""
@@ -26,9 +23,3 @@ class User(Base):
 
     def __str___(self):
         return self.__repr__()
-
-    def as_dict(self):
-        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-
-
-Base.metadata.create_all(engine)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from .model import session, User
 from flask import Blueprint, request
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm.exc import UnmappedInstanceError
+from peewee import IntegrityError
+
+from .model import User
 
 bp = Blueprint('crud', __name__)
 
@@ -39,21 +39,19 @@ def create():
     try:
         user = request.get_json()
         validate_data(user)
-        db_user = User(**user)
-        session.add(db_user)
-        session.commit()
+        User.create(**user)
         return {'status': 201, 'mensagem': 'dados inseridos'}, 201
-    except IntegrityError as e:
+    except IntegrityError:
         return {
             'status': 409,
-            'mensagem': f'dados já existentes {e.orig}',
+            'mensagem': 'Email já registrado',
         }, 409
 
 
 @bp.route('/read', methods=['GET'])
 def read():
     """Retorna os dados dos usuários da banco de dados."""
-    query = session.query(User).all()
+    query = User.select().namedtuples()
     users = {
         user.id: {
             "username": user.username,
@@ -70,33 +68,31 @@ def read():
 def update(id):
     """Atualiza os dados de um usuário no banco de dados."""
     try:
-        user = session.query(User).get(id)
+        user = User.select().where(User.id == id)
         new_data = request.get_json()
         validate_data(new_data)
         user.username = new_data['username']
         user.email = new_data['email']
         user.password = new_data['password']
-        session.commit()
+        user.save()
         return {'status': 200, 'mensagem': 'dados atualizados'}, 200
-    except IntegrityError as e:
+    except IntegrityError:
         return {
             'status': 409,
-            'mensagem': f'dados já existentes {e.orig}',
+            'mensagem': 'Email já registrado',
         }, 409
-    except AttributeError:
+    except User.DoesNotExist:
         return {'status': 404, 'mensagem': 'id não encontrado'}, 404
 
 
 @bp.route('/delete/<int:id>', methods=['DELETE'])
 def delete(id):
     """Deleta um usuário do banco de dados."""
-    try:
-        user = session.query(User).get(id)
-        session.delete(user)
-        session.commit()
+    user = User.delete_by_id(id)
+    if user:
         return {'status': 200, 'mensagem': 'exluido com sucesso'}, 200
-    except UnmappedInstanceError:
-        return {'status': 404, 'mensagem': 'id não encontrado'}, 404
+
+    return {'status': 404, 'mensagem': 'id não encontrado'}, 404
 
 
 def init_app(app):
